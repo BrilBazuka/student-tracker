62d1da364d3a32e45d1987ba7f80728baf90f4f0
brilbazuka <andrew.bril@mail.ru>
2020-07-26 23:21:25 +0300

    add: mapping for updating student

--- src/main/java/by/bryl/studenttracker/controller/StudentController.java
+++ src/main/java/by/bryl/studenttracker/controller/StudentController.java
@@ -5,10 +5,7 @@ import by.bryl.studenttracker.service.IStudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -47,6 +44,13 @@ public class StudentController {
     public String addStudent(@ModelAttribute("student") Student student) {
         studentService.save(student);
         return "redirect:/student/list";
+    }
+
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("studentId") int id, Model model) {
+        Student student = studentService.getStudentById(id);
+        model.addAttribute("student", student);
+        return "update-student-form";
     }
 
 }
