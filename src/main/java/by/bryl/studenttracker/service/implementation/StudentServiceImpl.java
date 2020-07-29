af954210e42a0f512b7bda218ae1908004a8ad87
brilbazuka <andrew.bril@mail.ru>
2020-07-29 23:19:23 +0300

    add: some logging

--- src/main/java/by/bryl/studenttracker/service/implementation/StudentServiceImpl.java
+++ src/main/java/by/bryl/studenttracker/service/implementation/StudentServiceImpl.java
@@ -3,6 +3,8 @@ package by.bryl.studenttracker.service.implementation;
 import by.bryl.studenttracker.entity.Student;
 import by.bryl.studenttracker.repository.IStudentDao;
 import by.bryl.studenttracker.service.IStudentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,6 +20,8 @@ import java.util.List;
 @Service
 public class StudentServiceImpl implements IStudentService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(StudentServiceImpl.class);
+
     private IStudentDao studentDao;
 
     @Autowired
@@ -28,36 +32,48 @@ public class StudentServiceImpl implements IStudentService {
     @Override
     @Transactional
     public List<Student> getStudents() {
-        return studentDao.getStudents();
+        LOGGER.info("Getting all students");
+        List<Student> students = studentDao.getStudents();
+        LOGGER.info("Students have been successfully gotten");
+        return students;
     }
 
     @Override
     @Transactional
     public void save(Student student) {
+        LOGGER.info("Trying to save student " + student);
         studentDao.save(student);
+        LOGGER.info("Student " + student + " has been successfully saved");
     }
 
     @Override
     @Transactional
     public void deleteStudent(int id) {
+        LOGGER.info("Trying to delete student with id " + id);
         studentDao.deleteStudent(id);
+        LOGGER.info("Student with id " + id + " has been successfully deleted");
     }
 
     @Override
     @Transactional
     public Student getStudentById(int id) {
-        return studentDao.getStudentById(id);
+        LOGGER.info("Trying to get student with id " + id);
+        Student student = studentDao.getStudentById(id);
+        LOGGER.info("Got student " + student + " from a database");
+        return student;
     }
 
     @Override
     @Transactional
     public List<Student> searchStudents(String name) {
+        LOGGER.info("Trying to search students with first name or last name like " + name);
         List<Student> students;
         if (name != null && name.trim().length() > 0) {
             students = studentDao.searchStudents(name);
         } else {
             students = studentDao.getStudents();
         }
+        LOGGER.info("Searching has been successfully ended. Students found: " + students.size());
         return students;
     }
 }
